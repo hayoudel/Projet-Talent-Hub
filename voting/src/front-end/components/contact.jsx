a45d0c59b528f2b9370aed9d72da8a368936ea3d
hhouevome <hayoudel@gmail.com>
2024-06-05 15:22:37 -0400

    first projet

--- voting/src/front-end/components/contact.jsx
+++ voting/src/front-end/components/contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiPhoneCall, FiMail } from "react-icons/fi";
+import { AiOutlineEnvironment } from "react-icons/ai";
 import "../css/contact.css";
 
 function Contact() {
@@ -8,21 +9,30 @@ function Contact() {
             <div className="box">
                 <div className="info-section">
                     <div className="info-item">
-                        <div className="icon">
+                        <div className="icon1">
                             <FiPhoneCall  />
                         </div>
-                        <div className="text">
+                        <div className="text1">
                             <h3>TELEPHONE</h3>
                             <p>+225 0789873943</p>
                         </div>
                     </div>
                     <div className="info-item">
-                        <div className="icon">
+                        <div className="icon2">
                             <FiMail  />
                         </div>
-                        <div className="text">
+                        <div className="text2">
                             <h3>EMAIL</h3>
                             <p>contact@example.com</p>
+                        </div>
+                    </div>
+                    <div className="info-item">
+                        <div className="icon3">
+                        <AiOutlineEnvironment />
+                        </div>
+                        <div className="text3">
+                            <h3>LOCALISATION</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, nesciunt!</p>
                         </div>
                     </div>
                 </div>
